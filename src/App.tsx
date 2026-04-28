a1474e30b3f566e00d41bee39c61ab027d8ea970
Adrian Stancioiu <adrian.stancioiu96@yahoo.com>
2026-04-28 22:10:15 +0300

    - sixth commit - implemented Protected and Public Routes in App.tsx component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,30 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import NoteForm from './components/NoteForm'
 import EditNote from './components/EditNote'
 import { UserContextProvider } from './contexts/UserAuthenticationContextType'
+// import { PrivateRoute } from './privateRoutes/PrivateRoute'
+import { useUser } from './contexts/UserAuthenticationContextType'
+import { Navigate } from 'react-router-dom'
+import type { ReactElement } from 'react'
+
+function ProtectedRoute({ children }: { children: ReactElement }) {
+  const { userLogged } = useUser()
+
+  if (!userLogged?.token) {
+    return <Navigate to="/" replace />
+  }
+
+  return children
+}
+
+function PublicRoute({ children }: { children: ReactElement }) {
+  const { userLogged } = useUser()
+
+  if (userLogged?.token) {
+    return <Navigate to="/main" replace />
+  }
+
+  return children
+}
 
 function App() {
   return (
@@ -14,10 +38,14 @@ function App() {
         <BrowserRouter>
 
           <Routes>
-            <Route path="/" element={<Login />}></Route> //MUST name this path '/' -- this is the default path found by REACT Router
-            <Route path="/main" element={<Home />}></Route>
-            <Route path="/addNote" element={<NoteForm />}></Route>
-            <Route path="/editNote/:id" element={<EditNote />}></Route>
+            <Route path="/" element={<PublicRoute><Login /></PublicRoute>}></Route> //MUST name this path '/' -- this is the default path found by REACT Router
+            <Route path="/main" element={<ProtectedRoute><Home/></ProtectedRoute>}/>
+            <Route path="/addNote" element={<ProtectedRoute><NoteForm /></ProtectedRoute>}></Route>
+            <Route path="/editNote/:id" element={<ProtectedRoute><EditNote/></ProtectedRoute>}></Route>
+
+            {/* <Route element={<PrivateRoute/>}>
+
+            </Route> */}
           </Routes>
 
         </BrowserRouter>
---
+++ src/privateRoutes/PrivateRoute.tsx
@@ -0,0 +1,16 @@
+// import { Navigate, Outlet } from "react-router-dom";
+// import { useUser } from "../contexts/UserAuthenticationContextType";
+
+
+
+
+// export function PrivateRoute(){
+//     const {userLogged} = useUser();
+
+//     if(!userLogged){
+//         return <Navigate to="/" replace/>;
+//     }
+
+//     return <Outlet />
+    
+// }
